b1521902be527656fcf8a6da4a3c0136afe36fab
dexnis8 <dexnis30@gmail.com>
2025-03-29 12:44:40 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ function App() {
           <p className="text-lg mb-6">
             I just added an update.....Did you see me?
           </p>
+          <p className="text-lg mb-6">
+            This is update 2.....did you get me?
+          </p>
           <button className="px-4 py-2 bg-blue-600 text-white rounded-md">
             Get Started
           </button>
